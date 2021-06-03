dadea0959512f42b25f309f2668896bfa01864af
Arcyvilk <blazemakerrr@gmail.com>
2021-06-04 01:12:05 +0200

    :hammer: adding chrome plugin version

---
+++ chrome/plugin.js
@@ -0,0 +1,24 @@
+function customizeLegacyForumsUrl(url) {
+  const host = url.host?.replace('www', 'na').replace('forums.', '');
+  const server = host.replace('.leagueoflegends.com', '');
+  const pathname = url.pathname?.replace('board', server) ?? '';
+  const search = url.search?.replace('p', 't') ?? '';
+  const hash = url.hash ?? '';
+  const newUrl = `https://forums.leagueboards.net${pathname}${search}${hash}`;
+  return newUrl;
+}
+
+function listener(details) {
+  const url = new URL(details.url);
+  if (url && url.pathname.startsWith('/board'))
+    return {
+      redirectUrl: customizeLegacyForumsUrl(url)
+    };
+  else return {};
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  listener,
+  {urls: ["http://*.leagueoflegends.com/board/*"]},
+  ["blocking"]
+);
